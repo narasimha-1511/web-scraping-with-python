6683b5f4d4017910f6c810e643a56854994c43d6
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-11-13 20:06:35 +0530

    Updated the py project
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

--- index.py
+++ index.py
@@ -2,10 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 
-
-def get_product_details_snapdeal(url, user_agent):
+def get_product_details(url, user_agent):
     status_code = 503
 
     while status_code == 503:
@@ -15,16 +14,16 @@ def get_product_details_snapdeal(url, user_agent):
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, "html.parser")
 
-            title_element = soup.find("h1", {"class": "pdp-e-i-head"})
+            title_element = soup.find("h1", {"class": "pdp-e-i-head"}) or soup.find("span", {"id": "productTitle"})
             title = title_element.get_text().strip() if title_element else "Title not found"
 
-            price_element = soup.find("span", {"class": "pdp-final-price"})
+            price_element = soup.find("span", {"class": "pdp-final-price"}) or soup.find("span", {"class": "a-price-whole"})
             price = price_element.get_text().strip() if price_element else "Price not found"
 
-            image_element = soup.find("img", {"class": "cloudzoom"})
+            image_element = soup.find("img", {"class": "cloudzoom"}) or soup.find("img", {"id": "landingImage"})
             image = image_element["src"] if image_element else "Image not found"
 
-            description_element = soup.find("div", {"class": "col-xs-18"})
+            description_element = soup.find("div", {"class": "col-xs-18"}) or soup.find("div", {"id": "productDescription"})
             description = description_element.get_text().strip() if description_element else "Description not found"
             status_code = 200
 
@@ -35,72 +34,33 @@ def get_product_details_snapdeal(url, user_agent):
                 "description": description
             }
         else:
-            print("Failed to retrieve Snapdeal product details. Retrying...")
+            print(f"Failed to retrieve product details. Retrying for URL: {url}")
 
-
-def get_product_details_amazon(url, user_agent):
-    status_code = 503
-
-    while status_code == 503:
-        headers = {"User-Agent": user_agent}
-        response = requests.get(url, headers=headers)
-
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, "html.parser")
-
-            title_element = soup.find("span", {"id": "productTitle"})
-            title = title_element.get_text().strip() if title_element else "Title not found"
-
-            price_element = soup.find("span", {"class": "a-price-whole"})
-            price = price_element.get_text().strip() if price_element else "Price not found"
-
-            image_element = soup.find("img", {"id": "landingImage"})
-            image = image_element["src"] if image_element else "Image not found"
-
-            description_element = soup.find("div", {"id": "productDescription"})
-            description = description_element.get_text().strip() if description_element else "Description not found"
-            status_code = 200
-
-            return {
-                "title": title,
-                "price": price,
-                "image": image,
-                "description": description
-            }
-        else:
-            print("Failed to retrieve Amazon product details. Retrying...")
-    
-
-def compare_prices(url_snapdeal, url_amazon):
-    snapdeal_details = get_product_details_snapdeal(url_snapdeal, user_agent)
-    amazon_details = get_product_details_amazon(url_amazon, user_agent)
+def compare_prices(url1, url2, user_agent):
+    product1_details = get_product_details(url1, user_agent)
+    product2_details = get_product_details(url2, user_agent)
 
     try:
-        snapdeal_price = float(snapdeal_details["price"].replace("Rs.", "").replace(",", ""))
+        product1_price = float(product1_details["price"].replace("Rs.", "").replace(",", ""))
     except ValueError:
-        snapdeal_price = float("inf")
+        product1_price = float("inf")
 
     try:
-        amazon_price = float(amazon_details["price"].replace("Rs.", "").replace(",", ""))
+        product2_price = float(product2_details["price"].replace("Rs.", "").replace(",", ""))
     except ValueError:
-        amazon_price = float("inf")
+        product2_price = float("inf")
 
-
-    if amazon_price < snapdeal_price:
-        return amazon_details, url_amazon
+    if product2_price < product1_price:
+        return product2_details, url2
     else:
-        return snapdeal_details, url_snapdeal
-
-    return None, None
-
+        return product1_details, url1
 
 if __name__ == "__main__":
-    snapdeal_url = input("Enter Snapdeal Product Link:")
-    amazon_url = input("Enter amazon Product Link:")
-    cheaper_product, product_url = compare_prices(snapdeal_url, amazon_url)
+    url1 = input("Enter the first product URL: ")
+    url2 = input("Enter the second product URL: ")
+    cheaper_product, product_url = compare_prices(url1, url2, USER_AGENT)
 
     if cheaper_product:
-
         table = [["Field", "Value"],
                  ["Title", cheaper_product["title"]],
                  ["Price", cheaper_product["price"]],
